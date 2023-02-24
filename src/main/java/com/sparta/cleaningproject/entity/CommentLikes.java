97ea55e9d70c3d30bdd8c558287bca75e1735dc3
Kuno1734 <dlsgus59@gmail.com>
2023-02-24 17:21:16 +0900

    feat : CommentLikes 엔티티 추가

---
+++ src/main/java/com/sparta/cleaningproject/entity/CommentLikes.java
@@ -0,0 +1,29 @@
+package com.sparta.cleaningproject.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+    @Getter
+    @Entity
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public class CommentLikes {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @ManyToOne
+        @JoinColumn(name = "USERS_ID", nullable = false)
+        private User user;
+
+        @ManyToOne
+        @JoinColumn(name = "COMMENT_ID")
+        private Comment comment;
+
+        public CommentLikes(User user, Comment comment) {
+            this.user = user;
+            this.comment = comment;
+        }
+    }
+    
