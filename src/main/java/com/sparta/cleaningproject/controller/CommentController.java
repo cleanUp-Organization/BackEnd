158b4e80b8f6e3a7858ca115ce1bba3b2f172afe
Kuno1734 <dlsgus59@gmail.com>
2023-02-24 13:51:27 +0900

    feat : controller package 생성 , CommentController생성 		댓글 작성 [ Post : createComment추가 ] 	 service package생성 , CommentService생성

---
+++ src/main/java/com/sparta/cleaningproject/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.sparta.cleaningproject.controller;
+
+import com.sparta.cleaningproject.dto.MessageResponseDto;
+import com.sparta.cleaningproject.security.UserDetailsImpl;
+import com.sparta.cleaningproject.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("/comment/{id}")
+    public ResponseEntity<MessageResponseDto> createComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return commentService.createComment(id,requestDto,userDetails.getUser());
+    }
+}
---
+++ src/main/java/com/sparta/cleaningproject/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.sparta.cleaningproject.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+}
