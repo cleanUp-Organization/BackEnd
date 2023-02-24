39b143b6e9c44cbf99693a7aca392be472e7704e
Kuno1734 <dlsgus59@gmail.com>
2023-02-24 13:59:48 +0900

    feat: CommentRepository 추가

--- src/main/java/com/sparta/cleaningproject/service/CommentService.java
+++ src/main/java/com/sparta/cleaningproject/service/CommentService.java
@@ -1,10 +1,12 @@
 package com.sparta.cleaningproject.service;
 
-import lombok.Getter;
+import com.sparta.cleaningproject.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
 }
