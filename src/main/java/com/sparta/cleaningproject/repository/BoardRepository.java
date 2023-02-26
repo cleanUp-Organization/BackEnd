387fd676ea5e2165c31e2b2314703f0470e5233e
Seyoung Oh <osy9536@naver.com>
2023-02-26 21:38:19 +0900

    fix : 회원탈퇴 수정
    회원탈퇴시 연관관계 매핑이 돼있어서 자식 테이블들을 삭제시키고 유저테이블 삭제

--- src/main/java/com/sparta/cleaningproject/repository/BoardRepository.java
+++ src/main/java/com/sparta/cleaningproject/repository/BoardRepository.java
@@ -2,10 +2,12 @@ package com.sparta.cleaningproject.repository;
 
 
 import com.sparta.cleaningproject.entity.Board;
+import com.sparta.cleaningproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
     List<Board> findAllByOrderByCreatedAtDesc();
+    void deleteAllByUser(User user);
 }
